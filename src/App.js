580ea6d211778b409e6b1f94a493fcd1e99ec9ba
Dillon Craw <68667382+crawnic311@users.noreply.github.com>
2023-12-15 06:23:16 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import ProjectCarousel from './components/Projects/ProjectCarousel'
 import ContactHeadline from './components/Get-In-Touch/contact-headline'
 import Contact from './components/Get-In-Touch/contact'
 import SkillsHeadline from './components/Skills/skills-headline'
-import Menu from './components/Headline/menu'
-import MenuToggle from './components/Menu/menu-toggle'
 import AboutMeHeadline from './components/About-Me/about-me-headline'
 
 function App() {
@@ -40,19 +38,19 @@ function App() {
       id: 2,
       title: 'Disaster Discovery Center',
       image: '/DDC.png',
-      link: 'https://disasterdiscoverycenter.org/',
+      link: 'https://docs.google.com/presentation/d/1pf1v125gWrJbA51xsBJGbjDQeeTKLAs7FX0P4GUObGc/edit?usp=sharing',
       description:
         'An in person workshop to help individuals and families develop awareness around their emergency preparedness.',
-      info: 'I worked with a team to create this workshop that is now used on college campuses across the US to help students experience and prepare for the challenges of natural disasters in their area.',
+      info: 'The Disaster Discovery Center project was one of my favorite projects during my time in graduate school. Our team was tasked with creating an in person training course that would help college students have an immersive and informative experience about how to be prepared for natural disasters that would be likely to occur in their area. Participants would navigate this course using a mobile app on their phones which we created. My primary role in this project was to design the accompanying mobile app, including everything from the entire UI to the instructional videos and materials contained within it. Most of the materials and content we produced for the DDC is not publicly available but I have linked a google slide that documents the mobile app design materials and files I made for this project.',
     },
     {
       id: 3,
       title: 'Doko Recycling',
       image: '/doko.jpg',
-      link: 'https://dokorecyclers.com/',
+      link: 'https://drive.google.com/drive/folders/14uEBsWlS7JexFK2DOzLWzFe2mw_CiSI-?usp=sharing',
       description:
         'A public school curriculum developer for students throughout Kathmandu, Nepal learn all about recycling.',
-      info: 'I lead a team of locals and humanitarian volunteers to develop this comprehensive high school curriculum for the NGO Doko Recyclers in Kathmandu, Nepal.',
+      info: 'Working with DOKO Recyclers was truly the experience of a lifetime! This was a project I was heavily involved in while on a self funded humanitarian trip in Nepal. DOKO is a local NGO who asked us to create a a curriculum on recycling for them to use in the established school systems throughout Kathmandu, Nepal. I lead a team of volunteers and educators through the process of designing an entire curriculum plan from scratch. We met multiple times a week with DOKO to better understand their vision and the unique cultural needs of the native students. This was a difficult project but ultimately an enriching and exciting experience which ultimately helped me decide to pursue a masters degree in instructional psychology and technology. Doko used our curriculum plan and adapted it more specifically to the plethora of educational institutions they are now partnered with. However, their curriculum is not publicly available. I have linked access to a google folder containing the lesson structure, curriculum plan, curriculum outline, and learning activities which my team and I created for DOKO.',
     },
     {
       id: 4,
@@ -61,24 +59,15 @@ function App() {
       link: 'https://open.byu.edu/50_years',
       description:
         'The publication is an open education resource synthesizing the past 50 years of research in the education and technology.',
-      info: 'I wrote the chapter on Educational Technology in the 1980 decade and was heavily involved in the reviewing, editing, and writing of the rest of the Educational Technology section.  ',
+      info: 'This 50 years of education research book is an open education resource book that one of my graduate school professors initiated. I had  the special opportunity to write the chapter on Educational Technology in the 1980 decade and was heavily involved in the reviewing, editing, and writing of the rest of the Educational Technology section. The image here links directly to the published book.',
     },
     {
       id: 5,
-      title: 'Handyman Training',
-      image: '/Handyman.jpg',
-      link: '',
-      description:
-        'A simple online presentation and course designed to help anyone get their general handyman business started.',
-      info: 'I created this course in both adobe captivate and articulate storyline 360. It features audio and vidual presentation, mini-tests, and gamified learning.',
-    },
-    {
-      id: 6,
       title: 'Minesweeper',
       image: '/Minesweeper Preview.jpeg',
       link: 'https://minesweeper-1bfe6.web.app/',
       description: 'My take on the Microsoft classic - Minesweeper.',
-      info: 'Built using pure JavaScript, HTML, CSS, and deployed with Firebase. It features recursive cell revealing and an immersive interface. Give it a go!',
+      info: 'Built using pure JavaScript, HTML, CSS, and deployed with Firebase. It features recursive cell revealing and an immersive interface. This is meant to display my technical skills as well as showcase my abilities as a designer and computer programmer.',
     },
   ]
 
@@ -95,9 +84,6 @@ function App() {
         alt=""
         className="BG-Image"
       />
-      {/* <Menu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} /> */}
-      {/* {isMenuOpen ? <MenuToggle menuScreen={menuScreen} /> : <div></div>} */}
-      {/* <MenuToggle menuScreen={menuScreen} /> */}
       <div className="App-Headline-Section" id="Headline-BG-Image">
         <a className="Logo" href=""></a>
         <Headline />
